9273aa71f77343781ab1ad6f1a3be1180dd07a98
Nil Jimenez <nil.jimenez@estudiant.enti.cat>
2025-11-27 11:31:55 +0100

    E21 to E22

--- Assets/E22/E22.cs
+++ Assets/E22/E22.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.PlayerSettings;
 
-public class E21 : MonoBehaviour
+public class E22 : MonoBehaviour
 {
 
     public float speed;
+    public float speedTarget;
     public GameObject target;
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +22,10 @@ public class E21 : MonoBehaviour
     {
 
         Vector3 dir = new Vector3(0, 0, 0);
+
+       
+
         
-
-
 
         if (Keyboard.current.wKey.isPressed)
         {
@@ -41,11 +45,16 @@ public class E21 : MonoBehaviour
         if (Keyboard.current.dKey.isPressed)
         {
             dir.x = 1;
-
         }
+
         transform.position += dir * speed * Time.deltaTime;
 
-        target.transform.position = transform.position;
+       
+        
+        target.transform.position = Vector3.Lerp(target.transform.position, transform.position, 2 * Time.deltaTime);
+            
+        
 
+        
     }
 }
